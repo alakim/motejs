278a600beb3ca5ac21eace3b61147fa07f3753ec
alvakim <alvakim@gmail.com>
2014-02-27 11:45:45 +0000

    Изменен алгоритм падения

--- mote.js
+++ mote.js
@@ -1,4 +1,11 @@
 ﻿var Mote = (function($,$H,$R){
+	
+	function getCoordAttrNames(icon){
+		if(!icon.attrs) return ["x", "y"];
+		return icon.attrs.y?["x","y"]:
+				icon.attrs.cy?["cx","cy"]:
+				["x","y"]
+	}
 
 	function screen(panel){
 		if(panel.jquery) panel = panel[0];
@@ -7,7 +14,7 @@
 		paper.customAttributes.gravityProgress = function (v) {
 			var data = this.data("fallData");
 			var t = data.duration*v;
-			this.attr({y:data.pos0.y + data.acceleration*t*t/2});
+			this.attr({transform:"t0,"+(data.acceleration*t*t/2)});
 		}
 		
 		var instance = {
@@ -37,12 +44,15 @@
 				return obj;
 			},
 			gravity:{
-				acceleration: .0002,
+				acceleration: .001,
 				fall: function(obj, height){
 					var a = obj.screen.gravity.acceleration;
 					var duration = Math.sqrt(height*2/a);
+					var attNames = getCoordAttrNames(obj.icon);
+					var bRect = obj.icon.getBBox();
+					console.log(bRect);
 					obj.icon.data("fallData", {
-						pos0:{x:obj.icon.attr("x"), y:obj.icon.attr("y")},
+						pos0:{x:bRect.x, y:bRect.y},
 						height: height,
 						duration: duration,
 						acceleration: a
@@ -57,7 +67,7 @@
 	}
 	
 	
-	function moteObject(pos, template){
+	function Object(pos, template){
 		if(!pos) pos = {x:0, y:0};
 		else if(pos instanceof Array) pos = {x:pos[0], y:pos[1]};
 		
@@ -79,6 +89,6 @@
 	return {
 		version:"1.0",
 		screen: screen,
-		object: moteObject
+		object: Object
 	};
 })(jQuery, Html, Raphael);
