4298fd48e58094bae523bb5d8577722ac20d2555
alvakim <alvakim@gmail.com>
2014-02-28 08:50:58 +0000

    Новая терминология - world, solid, screen

--- mote.js
+++ mote.js
@@ -7,96 +7,96 @@
 				["x","y"]
 	}
 
-	function screen(panel, template){
+	function World(panel, template){
 		if(panel.jquery) panel = panel[0];
-		var paper = new $R(panel);
+		var screen = new $R(panel);
 		
-		paper.customAttributes.gravityProgress = function (v) {
+		screen.customAttributes.gravityProgress = function (v) {
 			var data = this.data("fallData");
 			var t = data.duration*v;
 			this.attr({transform:"t0,"+(data.acceleration*t*t/2)});
 		}
 		
 		var instance = {
-			add: function(obj){
-				var icon = obj.template(paper, obj.position);
+			add: function(solid){
+				var icon = solid.template(screen, solid.spawnPosition);
 				icon.data("myset", icon);
-				obj.icon = icon;
-				obj.screen = instance;
+				solid.icon = icon;
+				solid.world = instance;
 				
 				icon.drag(
 					function(dx, dy, x, y, e) {//move
-						if(obj.isStatic)return;
+						if(solid.isStatic)return;
 						var myset = this.data("myset");
 						myset.transform(this.data("mytransform")+'T'+dx+','+dy);
 					},
 					function(x, y, e) {//start
-						if(obj.isStatic)return;
+						if(solid.isStatic)return;
 						var myset = this.data("myset");
 						myset.data("mytransform", this.transform());
 					},
 					function(e) {//end
-						if(obj.isStatic)return;
+						if(solid.isStatic)return;
 						var myset = this.data("myset");
 						myset.data("mytransform", this.transform());
-						obj.fall();
+						solid.fall();
 					}
 				);
-				return obj;
+				return solid;
 			},
 			gravity:{
 				acceleration: .001,
 				groundPosition: 450,
-				getHeight: function(obj){
-					var groundPos = obj.screen.gravity.groundPosition;
-					return groundPos - obj.icon.getBBox().y2;
+				getHeight: function(solid){
+					var groundPos = solid.world.gravity.groundPosition;
+					return groundPos - solid.icon.getBBox().y2;
 				},
-				fall: function(obj){
-					if(obj.isStatic) return;
-					var height = obj.screen.gravity.getHeight(obj);
-					var a = obj.screen.gravity.acceleration;
+				fall: function(solid){
+					if(solid.isStatic) return;
+					var height = solid.world.gravity.getHeight(solid);
+					var a = solid.world.gravity.acceleration;
 					var duration = Math.sqrt(height*2/a);
-					var attNames = getCoordAttrNames(obj.icon);
-					var bRect = obj.icon.getBBox();
-					obj.icon.data("fallData", {
+					var attNames = getCoordAttrNames(solid.icon);
+					var bRect = solid.icon.getBBox();
+					solid.icon.data("fallData", {
 						pos0:{x:bRect.x, y:bRect.y},
 						height: height,
 						duration: duration,
 						acceleration: a
 					});
 					
-					obj.icon.attr("gravityProgress", 0);
-					obj.icon.animate({ gravityProgress: 1 }, duration);
+					solid.icon.attr("gravityProgress", 0);
+					solid.icon.animate({ gravityProgress: 1 }, duration);
 				}
 			}
 		};
-		template(instance, paper)
+		template(instance, screen)
 		return instance;
 	}
 	
 	
-	function Object(pos, template){
+	function Solid(pos, template){
 		if(!pos) pos = {x:0, y:0};
 		else if(pos instanceof Array) pos = {x:pos[0], y:pos[1]};
 		
 		if(!template || typeof(template)!="function") 
-			template = function(paper, pos){
-				return paper.rect(pos.x, pos.y, 10, 10).attr({fill:"#ffc", stroke:"#448"});
+			template = function(screen, pos){
+				return screen.rect(pos.x, pos.y, 10, 10).attr({fill:"#ffc", stroke:"#448"});
 			}
 		return {
-			position: pos,
+			spawnPosition: pos,
 			template: template,
 			isStatic: false,
 			"static": function(v){this.isStatic = v==null?true:v; return this;},
 			fall: function(){
-				this.screen.gravity.fall(this);
+				this.world.gravity.fall(this);
 			}
 		};
 	}
 
 	return {
 		version:"1.0",
-		screen: screen,
-		object: Object
+		world: World,
+		solid: Solid
 	};
 })(jQuery, Html, Raphael);
