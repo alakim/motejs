3019bfad4eb8e6c44a3ff419667bc502a4bdab02
alvakim <alvakim@gmail.com>
2014-02-28 13:15:16 +0000

    Отработка начальной скорости (пока только по горизонтали) в процессе падения

--- mote.js
+++ mote.js
@@ -27,6 +27,20 @@
 		}
 	});
 	
+	function Velocity(dx, dy){
+		this.dx = dx||0;
+		this.dy = dy||0;
+	}
+	$.extend(Velocity.prototype, {
+		set: function(dx, dy){
+			this.dx = dx;
+			this.dy = dy;
+		},
+		toString: function(){
+			return [this.dx, this.dy].join();
+		}
+	});
+	
 	function World(panel, template){
 		if(panel.jquery) panel = panel[0];
 		var screen = new $R(panel);
@@ -36,7 +50,9 @@
 				fallState = solid.fallState,
 				t = fallState.duration*v,
 				dy = fallState.acceleration*t*t/2;
-			solid.transformation = fallState.baseTransform.shift(0, dy);
+			//console.log(solid.velocity.dx);
+			//solid.transformation = fallState.baseTransform.shift(solid.velocity.dx*t, dy+solid.velocity.dy*t);
+			solid.transformation = fallState.baseTransform.shift(solid.velocity.dx*t, dy);
 			solid.icon.attr({transform:solid.transformation});
 		}
 		
@@ -50,7 +66,15 @@
 				icon.drag(
 					function(dx, dy, x, y, e) {//move
 						if(solid.isStatic)return;
+						var kVel = .03;
+						var x0 = solid.transformation.T.x,
+							y0 = solid.transformation.T.y;
 						solid.transformation = solid.drag.baseTransform.shift(dx, dy);
+						solid.velocity.set(
+							(solid.transformation.T.x - x0)*kVel,
+							(solid.transformation.T.y - y0)*kVel
+						);
+						//console.log(solid.velocity+"");
 						solid.icon.attr({transform: solid.transformation});
 					},
 					function(x, y, e) {//start
@@ -80,6 +104,10 @@
 					var height = solid.world.gravity.getHeight(solid),
 						a = solid.world.gravity.acceleration(height),
 						duration = Math.sqrt(height*2/a),
+						//%%%% a*t*t/2 + u*t - s = 0
+						//%%%% t = (-u + Math.sqrt(u*u + 4*a*s))/(2*a)
+						// u = solid.velocity.dy;
+						// duration = (-u + Math.sqrt(u*u + 4*a*height))/(2*a);
 						bRect = solid.icon.getBBox();
 						
 					solid.fallState = {
@@ -90,7 +118,9 @@
 					};
 					
 					solid.icon.attr("gravityProgress", 0);
-					solid.icon.animate({ gravityProgress: 1 }, duration);
+					solid.icon.animate({ gravityProgress: 1 }, duration, function(){
+						solid.velocity.set(0, 0);
+					});
 				}
 			}
 		};
@@ -110,6 +140,7 @@
 		return {
 			spawnPosition: pos,
 			transformation: new Transformation(),
+			velocity: new Velocity(),
 			template: template,
 			isStatic: false,
 			"static": function(v){this.isStatic = v==null?true:v; return this;},
