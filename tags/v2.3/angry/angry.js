bf7712d71b753607157ccd47c69e012d3d68d6ec
alvakim@gmail.com <alvakim@gmail.com@a2e99bb1-819b-28d3-3b5b-a5d6b4ba60b0>
2014-03-13 12:18:30 +0000

    Mote v.2.3 Angry v.1.1 Работающая катапульта

---
+++ tags/v2.3/angry/angry.js
@@ -0,0 +1,155 @@
+﻿var Angry = (function($,$R,$M){
+	var attColor = {fill:"#ccc", stroke:"#888"},
+		height = 150,
+		width = 100,
+		basketSize = 15,
+		basketWidth = basketSize*2,
+		elasticModulus = .0002;
+		
+	var basket, basketIcon, tape;
+	
+	function animate(){
+		with(basketIcon){
+			var dt = +new Date - data("time"),
+				tension = data("tension"),
+				angle = data("angle"),
+				angleRad = $R.rad(angle);
+				velocity = data("velocity");
+		}
+		var m = 1,
+			a = tension/m,
+			dx = dt*velocity.vx,
+			dy = dt*velocity.vy,
+			dVx = a*dt*Math.cos(angleRad),
+			dVy = a*dt*Math.sin(angleRad),
+			targetPoint = basketIcon.data("targetPoint");
+			
+		velocity.accelerate(dVx, dVy);
+		var tapePath = tape.attr("path");
+		
+		tapePath[1][1] += dx;
+		tapePath[1][2] += dy;
+		if(tapePath[1][1]>targetPoint.x || tapePath[1][2]<targetPoint.y){
+			tapePath[1][1] = targetPoint.x;
+			tapePath[1][2] = targetPoint.y;
+		}
+		tape.attr("path", tapePath);
+		
+		moveBasketToTape(angle);
+		
+		tension = ($R.getTotalLength(tapePath) - width)*elasticModulus;
+		
+		if(tapePath[1][1]<targetPoint.x && tension>0){
+			basketIcon.data("tension", tension);
+			$M.requestAnimFrame()(animate);
+		}
+		else{
+			basket.updateBBox();
+			if(basket.accepted){with(basket){
+				var v = icon.data("maxTension")*m*50;
+				accepted.velocity.set(v*Math.cos(angleRad), v*Math.sin(angleRad));
+				accepted.falling = true;
+				accepted.fall();
+				accepted = null;
+			}}
+		}
+	}
+	
+	function moveBasketToTape(angle){
+		var tapePath = tape.attr("path"),
+			tapeEnd = {
+				x: tapePath[1][1],
+				y: tapePath[1][2]
+			};
+		var bPos = {
+				x: basketIcon.attr("x"),
+				y: basketIcon.attr("y")
+			},
+			bTr = $M.Transformation.obtain(basketIcon);
+		
+		bTr.T.x = tapeEnd.x - bPos.x - basketWidth;
+		bTr.T.y = tapeEnd.y - bPos.y - basketSize/2;
+		bTr.R = [angle, tapeEnd.x, tapeEnd.y];
+		basketIcon.transform(bTr);
+	}
+	
+	function moveAccepted(solid, x, y, angle){
+		var trf = $M.Transformation.obtain(solid.icon);
+		var dx = x - solid.spawnPosition.x,
+			dy = y - solid.spawnPosition.y;
+		trf = new $M.Transformation(dx, dy, angle);
+		solid.icon.transform(trf);
+	}
+		
+	function draggableBasket(basket){
+		var drag2 = {
+			move: function(dx, dy, x, y, e) {
+				var trf = $M.Transformation.obtain(this);
+				
+				var basePoint = this.data("basePoint"),
+					x0 = this.attr("x"),
+					y0 = this.attr("y"),
+					x1 = x0 + trf.T.x + basketWidth,
+					y1 = y0 + trf.T.y + basketSize/2,
+					alpha = $R.angle(basePoint.x, basePoint.y, x1, y1);
+					
+				trf.R = [alpha, x1, y1];
+				this.transform(trf);
+				var accepted = this.data("solid").accepted;
+				if(accepted){
+					var bbox = this.getBBox();
+					moveAccepted(accepted, bbox.x, bbox.y - basketSize/2, [alpha, bbox.x, bbox.y - basketSize/2]);
+				}
+				
+				var path = tape.attr("path");
+				path[1][1] = x1;
+				path[1][2] = y1;
+				tape.attr({path:path});
+				
+				var tension = ($R.getTotalLength(path) - width)*elasticModulus
+				this.data("maxTension", tension);
+				this.data("tension", tension);
+				this.data("angle", alpha);
+			},
+			start:function(x, y, e) {
+			},
+			end: function(e) {
+				var tension = this.data("tension");
+				this.data("time", +new Date);
+				this.data("velocity", new $M.Velocity);
+				var targetPoint = $R.getPointAtLength(tape.attr("path"), width);
+				this.data("targetPoint", targetPoint);
+				$M.requestAnimFrame()(animate);
+			}
+		};
+		$.extend(basket.drag, drag2);
+	}
+	
+	
+	function Gun(world, pos, template){
+		if(pos instanceof Array) pos = {x:pos[0], y:pos[1]};
+		template = template || function(screen, pos){
+			var support = screen.rect(pos.x, pos.y-height, 5, height).attr(attColor);
+			tape = screen.path(["M", pos.x, pos.y-height, "l", -width+basketWidth, 0]);
+			return support;
+		};
+		var gun = $M.solid(pos, 1e7, template).static();
+		world.add(gun);
+		basket = world.add($M.solid(pos, 1, function(screen, pos){
+			return screen.rect(pos.x-width, pos.y-height-basketSize/2, basketWidth, basketSize).attr({fill:"#ccc", stroke:"#888"});
+		}));
+		basket.falling = false;
+		draggableBasket(basket);
+		basketIcon = basket.icon;
+		basketIcon.data("basePoint", {x:pos.x, y:pos.y-height});
+		basket.accept = function(solid){
+			solid.falling = false;
+			basket.accepted = solid;
+		}
+	}
+	
+	return {
+		version: "1.1",
+		gun: Gun
+	};
+})(jQuery, Raphael, Mote);
---
+++ tags/v2.3/mote.js
@@ -0,0 +1,354 @@
+﻿var Mote = (function($,$H,$R){
+	
+	var getUID = (function(){
+		var counter = 0;
+		return function(){
+			return counter++;
+		}
+	})();
+	
+	var requestAnimFrame = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) {setTimeout(callback, 16);};
+	
+	function Transformation(Tx, Ty, R){
+		this.T = {x:Tx||0, y:Ty||0};
+		this.R = R||0;
+	}
+	$.extend(Transformation.prototype, {
+		shift: function(dx, dy){
+			this.T.x+=dx;
+			this.T.y+=dy;
+			return this;
+		},
+		clone: function(){return new Transformation(this.T.x, this.T.y, this.R)},
+		toString: function(){
+			return "Tx,yRr".replace("x", this.T.x)
+				.replace("y", this.T.y)
+				.replace("r", this.R instanceof Array?this.R.join(","):this.R);
+		}
+	});
+	$.extend(Transformation, {
+		obtain: function(icon){
+			var Tx = 0, Ty = 0, R = 0;
+			var tAttr = icon.attr("transform");
+			$.each(tAttr, function(i, cmd){
+				if(cmd[0]=="T"){ 
+					Tx+=cmd[1];
+					Ty+=cmd[2];
+				}
+				else if(cmd[0]=="R"){
+					if(cmd.length==2) R = cmd[1]
+					else if(cmd.length==4) R = [cmd[1], cmd[2], cmd[3]];
+				}
+			});
+			return new Transformation(Tx, Ty, R);
+		}
+	});
+	
+	function Velocity(vx, vy){
+		this.vx = vx||0;
+		this.vy = vy||0;
+	}
+	$.extend(Velocity.prototype, {
+		set: function(vx, vy){
+			this.vx = vx;
+			this.vy = vy;
+		},
+		accelerate: function(dVx, dVy){
+			this.vx+=dVx;
+			this.vy+=dVy;
+		},
+		toString: function(){
+			return [this.vx, this.vy].join();
+		}
+	});
+	
+	function FallState(solid, velocity){
+		this.time = +new Date;
+		this.solid = solid;
+		this.baseTransform = solid.transformation;
+		this.velocity = velocity || solid.velocity || new Velocity();
+		this.acceleration = solid.world.gravity.acceleration(/*height*/);
+	}
+	
+	var Gravity = (function(){
+		var fallingSolids = [];
+		
+		function animationStep(){
+			var now = +new Date;
+			
+			for(var solid,C=fallingSolids,i=0; solid=C[i],i<C.length; i++){
+				var state = solid.fallState,
+					dt = now - state.time;
+				state.time = now;
+					
+				var dx = state.velocity.vx*dt,
+					dy = state.velocity.vy*dt;
+				solid.transformation.shift(dx, dy);
+				solid.bbox.x+=dx; solid.bbox.x2+=dx; solid.bbox.cx+=dx; 
+				solid.bbox.y+=dy; solid.bbox.y2+=dy; 
+				
+				state.velocity.accelerate(0, state.acceleration*dt);
+				
+				var ground = solid.getGroundPos();
+				
+				var screenWidth = solid.world.getScreen().width;
+				
+				var absPos = {
+					x: solid.transformation.T.x + solid.spawnPosition.x,
+					y: solid.transformation.T.y + solid.spawnPosition.y + solid.bbox.height
+				};
+				
+				function terminate(){
+					fallingSolids.splice(i--, 1); // TERMINATE FALLING
+					solid.icon.attr({transform:solid.transformation});
+					solid.updateBBox();
+				}
+				
+				if(absPos.y>=ground && solid.velocity.vy>0){
+					solid.transformation.T.y = ground - solid.spawnPosition.y - solid.bbox.height;
+					terminate();
+				}
+				else if(solid.world.reflect.top && absPos.y<0 && solid.velocity.vy<0){
+					solid.transformation.T.y = 0;
+					terminate();
+					solid.velocity.vx = state.velocity.vx;
+					solid.velocity.vy = -state.velocity.vy;
+					solid.fall();
+				}
+				else if(solid.world.reflect.left && absPos.x<0 && solid.velocity.vx<0){
+					solid.transformation.T.x = -solid.spawnPosition.x;
+					terminate();
+					solid.velocity.vx = -state.velocity.vx;
+					solid.velocity.vy = state.velocity.vy;
+					solid.fall();
+				}
+				else if(solid.world.reflect.right && absPos.x>screenWidth && solid.velocity.vx>0){
+					solid.transformation.T.x = screenWidth - solid.spawnPosition.x - solid.bbox.width;
+					terminate();
+					solid.velocity.vx = -state.velocity.vx;
+					solid.velocity.vy = state.velocity.vy;
+					solid.fall();
+				}
+				else{
+					solid.icon.attr({transform:solid.transformation});
+				}
+			}
+			fallingSolids.length && requestAnimFrame(animationStep);
+		}
+		
+		return {
+			acceleration: function(height){return .001;},
+			groundPosition: 450,
+			fall: function(solid){
+				if(solid.isStatic) return;
+				solid.fallState = new FallState(solid);
+				solid.updateBBox();
+				fallingSolids.push(solid);
+				fallingSolids.length && requestAnimFrame(animationStep);
+			}
+		};
+	})();
+	
+		
+	function draggable(solid){
+		var drag = {
+			move: function(dx, dy, x, y, e) {
+				if(solid.isStatic || !solid.drag.state)return;
+				var now = +new Date
+					dt = now - solid.drag.state.time;
+				solid.drag.state.time = now;
+				
+				var rate = .5;
+				var x0 = solid.transformation.T.x,
+					y0 = solid.transformation.T.y;
+				
+				solid.transformation = solid.drag.state.baseTransform.clone().shift(dx, dy);
+				solid.icon.attr({transform: solid.transformation});
+
+				solid.velocity.set(
+					(solid.transformation.T.x - x0)*rate/dt,
+					(solid.transformation.T.y - y0)*rate/dt
+				);
+				solid.drag.move.call(solid.icon, dx, dy, x, y, e);
+			},
+			start:function(x, y, e) {
+				if(solid.isStatic)return;
+				solid.drag.state = {
+					baseTransform: Transformation.obtain(this),
+					time: +new Date
+				};
+				solid.drag.start.call(solid.icon, x, y, e);
+			},
+			end: function(e) {
+				if(solid.isStatic)return;
+				solid.drag.state = null;
+				solid.updateBBox();
+				for(var sld,C=solid.world.solids,i=0; sld=C[i],i<C.length; i++){
+					if(sld===solid) continue;
+					if($R.isBBoxIntersect(solid.bbox, sld.bbox)){
+						solid.connect(sld);
+						sld.accept(solid);
+					}
+				}
+				solid.drag.end.call(solid.icon, e);
+				solid.fall();
+			}
+		};
+		solid.icon.drag(drag.move, drag.start, drag.end);
+	}
+	
+	function World(panel, template){
+		if(panel.jquery) panel = panel[0];
+		var screen = new $R(panel);
+		
+		var worldInstance = {
+			getScreen: function(){return screen;},
+			solids: [],
+			reflect:{top:1, left:1, right:1},
+			add: function(solid){
+				var icon = solid.template(screen, solid.spawnPosition);
+				icon.data("solid", solid);
+				solid.icon = icon;
+				solid.world = worldInstance;
+				solid.updateBBox();
+				solid.world.solids.push(solid);
+				draggable(solid);
+				return solid;
+			},
+			gravity: Gravity
+		};
+		template(worldInstance, screen);
+
+
+		return worldInstance;
+	}
+	
+	function deg2Rad (deg) { return deg / 180 * Math.PI; }
+	function rad2Deg (rad) { return rad / Math.PI * 180; }
+
+	// Возвращает разложение вектора в системе координат, 
+	// развернутой на угол angle (в градусах)
+	function vector(vx, vy, angle){
+		var abs = Math.sqrt(vx*vx + vy*vy),
+			vAngle = Math.atan(vx/vy) + deg2rad(angle); // угол вектора относительно новой системы координат
+		
+		return {
+			vx: vx, vy: vy,
+			// модуль вектора
+			abs: abs,
+			// нормальная составляющая
+			n: Math.sin(vAngle)*abs,
+			// продольная составляющая
+			t: Math.cos(vAngle)*abs
+		}
+	}
+	
+	function Collision(solid1, solid2, decrement){
+		var pos1 = solid1.getPosition(),
+			pos2 = solid2.getPosition();
+		var angle = $R.angle(pos1.x, pos1.y, pos2.x, pos2.y);
+
+		decrement = decrement || .5;
+		// var v = {
+		// 	vx: (solid1.velocity.vx + solid2.velocity.vx)*decrement,
+		// 	vy: (solid1.velocity.vy + solid2.velocity.vy)*decrement
+		// };
+		
+		var v1 = vector(solid1.velocity.vx, solid1.velocity.vy, angle),
+			v2 = vector(solid2.velocity.vx, solid2.velocity.vy, angle);
+			
+		
+	}
+	
+	function Solid(pos, mass, template){
+		if(!pos) pos = {x:0, y:0};
+		else if(pos instanceof Array) pos = {x:pos[0], y:pos[1]};
+		
+		if(!template || typeof(template)!="function") 
+			template = function(screen, pos){
+				return screen.rect(pos.x, pos.y, 10, 10).attr({fill:"#ffc", stroke:"#448"});
+			}
+		return {
+			id: getUID(),
+			spawnPosition: pos,
+			mass: mass,
+			transformation: new Transformation(),
+			getPosition: function(){
+				return {
+					x: this.spawnPosition.x + this.transformation.T.x,
+					x: this.spawnPosition.y + this.transformation.T.y
+				};
+			},
+			velocity: new Velocity(),
+			template: template,
+			fallState: null,
+			icon:null,
+			bbox:null,
+			isStatic: false,
+			"static": function(v){this.isStatic = v==null?true:v; return this;},
+			falling: true,
+			fall: function(){
+				if(!this.falling) return;
+				this.world.gravity.fall(this);
+			},
+			updateBBox: function(){
+				var b = this.icon.getBBox();
+				this.bbox = {
+					x: b.x,
+					x2: b.x2,
+					cx: (b.x2+b.x)/2,
+					y: b.y,
+					y2: b.y2,
+					width: b.width,
+					height: b.height
+				}
+			},
+			getGroundPos: function(){var _=this;
+				var cx = _.bbox.cx,
+					groundY = _.world.gravity.groundPosition;
+				for(var sld,C=_.world.solids,i=0; sld=C[i],i<C.length; i++){
+					if(sld===_ || !sld.bbox) continue;
+					if(cx>sld.bbox.x && cx<sld.bbox.x2){
+						if(sld.bbox.y<groundY)
+							groundY = sld.bbox.y;
+					}
+				}
+				return groundY;
+			},
+			traceBBox: function(color){var _=this;
+				var box = _.bbox,
+					color = color || "#f00",
+					screen = _.world.getScreen(),
+					attr = {stroke:color};
+				screen.rect(box.x, box.y, box.width, box.height).attr(attr);
+				screen.circle(box.x2, box.y2, 4).attr(attr);
+			},
+			connect: function(solid){
+				// console.log([this.id, " connected to ", solid.id]);
+			},
+			accept: function(solid){
+				// console.log([this.id, " accepts ", solid.id]);
+			},
+			drag:{
+				start: function(){},
+				move: function(){},
+				end: function(){}
+			}
+		};
+	}
+
+	return {
+		version:"2.3",
+		world: World,
+		solid: Solid,
+		getUID: getUID,
+		Transformation: Transformation,
+		Velocity: Velocity,
+		requestAnimFrame: function(){return requestAnimFrame;}
+	};
+})(jQuery, Html, Raphael);
