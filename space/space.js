8abf137ed22eda9c8564d64dd68b656bc0bc9536
alvakim <alvakim@gmail.com>
2014-03-20 13:04:57 +0000

    Gravity v1.1 пример с несколькими центрами тяготения

--- space/space.js
+++ space/space.js
@@ -2,29 +2,39 @@
 	
 	var counter = 0;
 	
+	function getAcceleration(star, solid, G){
+		/*******************
+			F = m*a;
+			F = G*m1*m2/(r*r);
+			a = G*M/(r*r);
+		********************/
+		var solPos = new $M.Vector(solid.transformation.T.x, solid.transformation.T.y),
+			stPos = new $M.Vector(star.transformation.T.x, star.transformation.T.y),
+			distance = stPos.add(solPos.mul(-1))
+			L = distance.getLength();
+		return new $M.Vector(distance).mul(1/L).mul(G*star.mass/(L*L));
+	}
+	
 	function Gravity(star){var _=this;
 		_.star = star;
 		_.G = 5e-2;
 		_.acceleration = function(solid){
 			//if(counter++>1000) throw "Execution stopped by developer";
-			/*******************
-				F = m*a;
-				F = G*m1*m2/(r*r);
-				a = G*M/(r*r);
-			********************/
-			var solPos = new $M.Vector(solid.transformation.T.x, solid.transformation.T.y),
-				stPos = new $M.Vector(star.transformation.T.x, star.transformation.T.y),
-				distance = stPos.add(solPos.mul(-1))
-				L = distance.getLength();
-			var a = new $M.Vector(distance).mul(1/L).mul(_.G*_.star.mass/(L*L));
-			return a;
+			if(_.star instanceof Array){
+				var a = new $M.Vector();
+				for(var s,i=0; s=_.star[i],i<_.star.length; i++){
+					a.add(getAcceleration(s, solid, _.G));
+				}
+				return a;
+			}
+			else return getAcceleration(_.star, solid, _.G);
 		}
 	}
 	Gravity.prototype = $M.Gravity;
 
 	
 	return {
-		version: "1.0",
+		version: "1.1",
 		Gravity: Gravity
 	};
 })(jQuery, Raphael, Mote);
