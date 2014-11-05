5d9e88e9c5c636b360d34c0e970789ccb885b04a
alvakim <alvakim@a2e99bb1-819b-28d3-3b5b-a5d6b4ba60b0>
2014-11-05 14:11:58 +0000

    Simple buglet icon with healt points

--- buglets/js/main.js
+++ buglets/js/main.js
@@ -1,4 +1,4 @@
-﻿requirejs.config({
+requirejs.config({
     baseUrl: "js",
     paths: {
 		jquery: "lib/jquery-1.11.0.min",
@@ -20,29 +20,37 @@ requirejs(["jquery", "html", "settings", "cpanel", "buglet", "field", "commands"
 	
 	(function(){ 
 		
-		// создание баглета
-		var bg1 = new Buglet("Our First Buglet", field, {x:25, y:55}, -50);
 		
-		// тестовый список команд
-		// как-то не красиво вызываются конструкторы команд
-		bg1.scheme.addCommand(new Cmd.MoveCmd({x:159, y:168}, bg1.scheme));
-		bg1.scheme.addCommand(new Cmd.MoveCmd({x:259, y:288}, bg1.scheme));
-		bg1.scheme.addCommand(new Cmd.MoveCmd({x:374, y:308}, bg1.scheme));
-		bg1.scheme.addCommand(new Cmd.MoveCmd({x:474, y:208}, bg1.scheme));
-		bg1.scheme.addCommand(new Cmd.MoveCmd({x:374, y:108}, bg1.scheme));
-		bg1.scheme.addCommand(new Cmd.MoveCmd({x:324, y:228}, bg1.scheme));
+		if(true){
+			// создание баглета
+			var bg1 = new Buglet("Our First Buglet", field, {x:25, y:55}, -50);
+			
+			// тестовый список команд
+			// как-то не красиво вызываются конструкторы команд
+			bg1.scheme.addCommand(new Cmd.MoveCmd({x:159, y:168}, bg1.scheme));
+			bg1.scheme.addCommand(new Cmd.MoveCmd({x:259, y:288}, bg1.scheme));
+			bg1.scheme.addCommand(new Cmd.MoveCmd({x:374, y:308}, bg1.scheme));
+			bg1.scheme.addCommand(new Cmd.MoveCmd({x:474, y:208}, bg1.scheme));
+			bg1.scheme.addCommand(new Cmd.MoveCmd({x:374, y:108}, bg1.scheme));
+			bg1.scheme.addCommand(new Cmd.MoveCmd({x:324, y:228}, bg1.scheme));
+			
+			bg1.show();
+		}
 		
-		bg1.show();
 		
-		
-		
-		var bg2 = new Buglet("Our Second Buglet", field, {x:325, y:55}, 130);
-		bg2.scheme.addCommand(new Cmd.MoveCmd({x:359, y:568}, bg2.scheme));
-		bg2.show();
+		if(true){
+			var bg2 = new Buglet("Our Second Buglet", field, {x:325, y:55}, 130);
+			bg2.scheme.addCommand(new Cmd.MoveCmd({x:359, y:568}, bg2.scheme));
+			bg2.show();
+		}
 		
 		var bg3 = new SimpleBuglet("Our Third Buglet", field, {x:425, y:255}, 30);
 		bg3.scheme.addCommand(new Cmd.MoveCmd({x:59, y:368}, bg3.scheme));
 		bg3.show();
+		
+		setTimeout(function(){
+			bg3.setHealth(.5);
+		}, 1500);
 		
 		var cpnl = new CPanel("cpanel"); // после создания всех баглетов
 
--- buglets/js/simplebuglet.js
+++ buglets/js/simplebuglet.js
@@ -1,5 +1,7 @@
 define(["jquery", "html", "oop", "buglet"], function($, $H, $OOP, Buglet){
 	
+
+	
 	function SimpleBuglet(name, field, pos, direction){
 		this.init(name, field, pos, direction);
 	}
@@ -7,6 +9,32 @@ define(["jquery", "html", "oop", "buglet"], function($, $H, $OOP, Buglet){
 	$OOP.inherit(SimpleBuglet, Buglet);
 
 	$OOP.mixin(SimpleBuglet.prototype, {
+		hpPath: function(val){var _=this;
+			val = val || 1;
+
+			var 	h2 = _.iconSize.h/2,
+				s = h2, // ширина скоса
+				hw = 3, // healthPoint width
+				hL = s*.7, // healthPoint left
+				mrg = 1; // margin - отступ от края
+			
+			var	pL = -_.iconSize.w/2+hL,
+				pR = pL+(_.iconSize.w-hL-s)*val,
+				pU = -h2+mrg,
+				pB = pU + hw;
+			return [
+				"M", pL, pU,
+				"L", pR, pU,
+				"L", pR, pB,
+				"L", pL, pB,
+				"z",
+				"M", pL, -pU,
+				"L", pR, -pU,
+				"L", pR, -pB,
+				"L", pL, -pB,
+				"z"
+			];
+		},
 		show: function(){var _=this;
 			_.icon = _.field.screen.set();
 			_.iconSize = {
@@ -17,10 +45,7 @@ define(["jquery", "html", "oop", "buglet"], function($, $H, $OOP, Buglet){
 			var 	w = _.iconSize.w,
 				w2 = _.iconSize.w/2,
 				h2 = _.iconSize.h/2,
-				s = h2, // ширина скоса
-				hw = 3, // healthPoint width
-				hL = s*.7, // healthPoint left
-				mrg = 1; // margin - отступ от края
+				s = h2; // ширина скоса
 			
 			// отрисовка пиктухи (относительно нуля)
  			_.icon.push(_.field.screen.path([
@@ -32,27 +57,9 @@ define(["jquery", "html", "oop", "buglet"], function($, $H, $OOP, Buglet){
 				"L", -w2+s, 0,
 				"z"
 			]).attr({fill:"#999"}));
-			
-			function hpPath(val){
-				val = val || 0;
-				var	pL = -w2+hL,
-					pR = pL+(w-hL-s)*val;
-				return [
-					"M", pL, -h2+mrg,
-					"L", pR, -h2+mrg,
-					"L", pR, -h2+mrg+hw,
-					"L", pL, -h2+mrg+hw,
-					"z",
-					"M", pL, h2-mrg,
-					"L", pR, h2-mrg,
-					"L", pR, h2-mrg-hw,
-					"L", pL, h2-mrg-hw,
-					"z"
-				];
-			}
-			
-			_.icon.push(_.field.screen.path(hpPath(1)).attr({fill:"#f00"}));
-			_.healthPoint = _.field.screen.path(hpPath(.8)).attr({fill:"#0f8"});
+						
+			_.icon.push(_.field.screen.path(_.hpPath()).attr({fill:"#f00"}));
+			_.healthPoint = _.field.screen.path(_.hpPath()).attr({fill:"#0f8"});
 			_.icon.push(_.healthPoint);
 			
 			
@@ -62,6 +69,9 @@ define(["jquery", "html", "oop", "buglet"], function($, $H, $OOP, Buglet){
 			// позиционирование пиктухи
 			_.icon.transform(["t", _.pos.x, _.pos.y, "r", _.posDirection(_.direction), -_.iconSize.w/2, -_.iconSize.h/2]);
 			
+		},
+		setHealth: function(val){var _=this;
+			_.healthPoint.attr({path:_.hpPath(val)});
 		}
 	});
 	
